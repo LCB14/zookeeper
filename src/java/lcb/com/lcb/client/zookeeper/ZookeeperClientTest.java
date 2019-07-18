f9e34318bc15ae8502a378cb86c1dfccc132365c
changbao.li <changbao.li@bianlifeng.com>
2019-07-18 21:56:43 +0800

    2019年07月18日21:56:42

--- src/java/lcb/com/lcb/client/zookeeper/ZookeeperClientTest.java
+++ src/java/lcb/com/lcb/client/zookeeper/ZookeeperClientTest.java
@@ -3,6 +3,7 @@ package com.lcb.client.zookeeper;
 import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.Watcher;
 import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.data.Stat;
 
 /**
  * @author changbao.li
@@ -13,12 +14,24 @@ public class ZookeeperClientTest {
 
     public static void main(String[] args) throws Exception {
 
+        /**
+         * zk原生客户端的wacth只能被使用一次
+         */
         ZooKeeper client = new ZooKeeper("localhost:2181", 5000, new Watcher() {
             @Override
             public void process(WatchedEvent event) {
                 System.out.println("连接" + event);
             }
         });
+
+        Stat stat = new Stat();
+        String str = new String(client.getData("/data", new Watcher() {
+            @Override
+            public void process(WatchedEvent event) {
+                System.out.println("事件类型：" + event.getType());
+            }
+        }, stat));
+        System.out.println("节点内容：" + str);
 
         System.in.read();
     }
