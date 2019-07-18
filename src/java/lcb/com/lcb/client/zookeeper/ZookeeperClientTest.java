257b4e6b7db1d33688ea563250f7c9cab9ebf81c
changbao.li <changbao.li@bianlifeng.com>
2019-07-18 22:08:39 +0800

    2019年07月18日22:08:37

--- src/java/lcb/com/lcb/client/zookeeper/ZookeeperClientTest.java
+++ src/java/lcb/com/lcb/client/zookeeper/ZookeeperClientTest.java
@@ -1,5 +1,6 @@
 package com.lcb.client.zookeeper;
 
+import org.apache.zookeeper.AsyncCallback;
 import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.Watcher;
 import org.apache.zookeeper.ZooKeeper;
@@ -25,14 +26,26 @@ public class ZookeeperClientTest {
         });
 
         Stat stat = new Stat();
+        // 使用自定义watch监听器
         String str = new String(client.getData("/data", new Watcher() {
             @Override
             public void process(WatchedEvent event) {
                 System.out.println("事件类型：" + event.getType());
             }
         }, stat));
-        System.out.println("节点内容：" + str);
 
+        // 使用自定义watch监听器
+        client.getData("/data", true, stat);
+
+        // 使用回调
+        client.getData("/data", false, new AsyncCallback.DataCallback() {
+            @Override
+            public void processResult(int rc, String path, Object ctx, byte[] data, Stat stat) {
+                System.out.println("method 被回调");
+            }
+        }, stat);
+
+        System.out.println("节点内容：" + str);
         System.in.read();
     }
 }
